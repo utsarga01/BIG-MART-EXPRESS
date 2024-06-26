5566f120555f4890bcb9f96333c437a8be6b214e
utsarga01 <regmiutsarga7@gmail.com>
2024-06-26 21:15:52 +0545

    prct

--- index.js
+++ index.js
@@ -15,25 +15,48 @@ let productList = [
   },
 ];
 // ?get product list
+// app.get("/product/list", (req, res) => {
+//   return res.status(200).send({ message: "success", productList });
+// });
 app.get("/product/list", (req, res) => {
   return res.status(200).send({ message: "success", productList });
 });
+
 // ? add product list
+// app.post("/product/add", (req, res) => {
+//   const newProduct = req.body;
+//   productList.push(newProduct);
+//   return res.status(200).send({ message: "Product is added successfully" });
+// });
+
+//adding product list
 app.post("/product/add", (req, res) => {
   const newProduct = req.body;
   productList.push(newProduct);
   return res.status(200).send({ message: "Product is added successfully" });
 });
 // ?get product detail by id
+// app.get("/product/detail/:id", (req, res) => {
+//   const productId = Number(req.params.id);
+
+//   const product = productList.find((item) => item.id === productId);
+//   if (!product) {
+//     return res.status(404).send({ message: "product doesnot exist" });
+//   }
+//   return res.status(200).send({ message: "Success", productDetails: product });
+// });
+
 app.get("/product/detail/:id", (req, res) => {
   const productId = Number(req.params.id);
-
   const product = productList.find((item) => item.id === productId);
   if (!product) {
-    return res.status(404).send({ message: "product doesnot exist" });
+    return res.status(404).send({ message: "Product doesnot exist" });
   }
+
   return res.status(200).send({ message: "Success", productDetails: product });
 });
+
+
 app.delete("/product/delete/:id", (req, res) => {
   // extract product id from req.params and convert to number type
   const productId = Number(req.params.id);

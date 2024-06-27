87faa82d63b8c73bf5b85e5896f7adadc1f4097a
utsarga01 <regmiutsarga7@gmail.com>
2024-06-27 22:01:43 +0545

    nosql

---
+++ sql.js
@@ -0,0 +1,19 @@
+// sql   
+// -table 
+// -row
+// -column
+// -record
+
+
+
+
+
+// nosql
+
+// -collection 
+// -Document
+// -field
+// -record
+// BSON (binary object notation)
+// -can also put structutred data
+
